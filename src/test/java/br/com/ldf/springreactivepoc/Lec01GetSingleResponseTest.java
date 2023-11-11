9b2bfae7fbad874497ded00ca780db304e0d858b
ice_lfernandes <ice_lfernandes@uolinc.com>
2023-11-10 23:55:58 -0300

    criação de teste mais correto para verificar de forma reativa

--- src/test/java/br/com/ldf/springreactivepoc/Lec01GetSingleResponseTest.java
+++ src/test/java/br/com/ldf/springreactivepoc/Lec01GetSingleResponseTest.java
@@ -4,6 +4,8 @@ import br.com.ldf.springreactivepoc.api.dto.response.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 public class Lec01GetSingleResponseTest extends BaseTest {
 
@@ -11,13 +13,24 @@ public class Lec01GetSingleResponseTest extends BaseTest {
     private WebClient webClient;
 
     @Test
-    public void blockTest() {
+    void blockTest() {
         var response = webClient.get()
                 .uri("reactive-math/square/{input}", 5)
                 .retrieve()
-                .bodyToMono(Response.class) // Mono<Response>
+                .bodyToMono(Response.class)
                 .block();
         System.out.println(response);
+    }
+
+    @Test
+    void stepVerifierTest() {
+        Mono<Response> response  = webClient.get()
+                .uri("reactive-math/square/{input}", 5)
+                .retrieve()
+                .bodyToMono(Response.class);
+        StepVerifier.create(response)
+                .expectNextMatches(r -> r.getOutput() == 25)
+                .verifyComplete();
     }
 
 }
